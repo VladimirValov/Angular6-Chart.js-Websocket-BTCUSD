de582bbbfe27ec9c5781151940cd6669197dbd57
Vladimir Valov <valov-web@yandex.ru>
2018-06-18 11:19:18 +0300

    change layout

--- src/app/modules/dashboard/components/layout/layout.component.ts
+++ src/app/modules/dashboard/components/layout/layout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { WebsocketService } from "src/app/services/websocket.service";
+// import {GeminiService } from "../../services/gemini.service"
 import { Gemini } from "src/app/types/gemini.d";
 import { SlicePipe } from "@angular/common";
 import { Subscription, Subject } from "rxjs";
@@ -12,7 +13,7 @@ import { Subscription, Subject } from "rxjs";
 export class LayoutComponent implements OnInit, OnDestroy {
   constructor(private websocket: WebsocketService) {}
 
-  public data: Gemini.Responce;
+  public rawData: Gemini.Responce;
   public btcUsd: Gemini.Event;
   public chart: Chart;
   private prices: number[];
@@ -22,41 +23,34 @@ export class LayoutComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.socket = this.websocket.connect();
-    console.log(this.socket);
-    // let i = 10;
-    // setInterval(() => {
-    //   if (!this.prices) {
-    //     this.prices = [i++];
-    //     this.times = ['02'];
-    //     this.btcUsd = {} as Gemini.Event;
-    //   }
-    //   this.prices.push(i++);
-    //   this.times.push('02' + i);
-    // }, 1000);
-    let time;
     this.socket.subscribe(res => {
       // console.log(res);
-      this.data = JSON.parse(res);
-      const change = this.data.events.find(ev => ev.type === "change");
-      time = new Date(this.data.timestampms).toLocaleTimeString();
-
-      // console.log(change);
-      if (change && change.side === "ask") {
-        this.btcUsd = change;
-        if (!this.prices) {
-          this.prices = Array(this.chartLength).fill(change.price);
-          this.times = Array(this.chartLength).fill(time);
-          return;
-        }
-        this.prices = [...this.prices, Number(change.price)].slice(
-          -this.chartLength
-        );
-        this.times = [...this.times, time].slice(-this.chartLength);
-      }
+      const data = JSON.parse(res);
+      this.generateChartData(data);
     });
   }
 
   ngOnDestroy() {
     this.socket.unsubscribe();
+  }
+
+  generateChartData (data: Gemini.Responce) {
+    const change = data.events.find(ev => ev.type === "change");
+    const time = new Date( data.timestampms).toLocaleTimeString();
+
+    // console.log(change);
+    if (change && change.side === "ask") {
+      this.btcUsd = change;
+      this.rawData = data;
+      if (!this.prices) {
+        this.prices = Array(this.chartLength).fill(change.price);
+        this.times = Array(this.chartLength).fill(time);
+        return;
+      }
+      this.prices = [...this.prices, Number(change.price)].slice(
+        -this.chartLength
+      );
+      this.times = [...this.times, time].slice(-this.chartLength);
+    }
   }
 }
